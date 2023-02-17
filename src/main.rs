2fe3a0dbd20073d6c95cf5d5d1b18b1ec045ae72
Alejandro Pascual Pozo <alejandro.pascual.pozo@gmail.com>
2023-02-17 11:29:00 +0100

    split modules

--- src/main.rs
+++ src/main.rs
@@ -1,82 +1,8 @@
-use std::{
-    fmt::{Debug, Formatter, Result},
-    hash::{Hash, Hasher},
-    marker::PhantomData,
-};
+mod system_command;
 
-use bevy::{
-    ecs::{schedule::ScheduleLabel, system::Command},
-    prelude::*,
-};
+use bevy::prelude::*;
 
-struct SystemCommand<T: 'static + Send + Sync>(T);
-
-impl<T: 'static + Send + Sync> Command for SystemCommand<T> {
-    fn write(self, world: &mut World) {
-        world.insert_resource(CommandIn(self.0));
-        world.run_schedule(CommandScheduleLabel::<T>::default());
-        world.remove_resource::<CommandIn<T>>();
-    }
-}
-
-#[derive(Resource)]
-struct CommandIn<T: 'static + Send + Sync>(T);
-
-#[derive(ScheduleLabel)]
-struct CommandScheduleLabel<T: 'static + Send + Sync> {
-    phantom_data: PhantomData<T>,
-}
-
-impl<T: 'static + Send + Sync> Default for CommandScheduleLabel<T> {
-    fn default() -> Self {
-        Self {
-            phantom_data: PhantomData,
-        }
-    }
-}
-
-impl<T: 'static + Send + Sync> Clone for CommandScheduleLabel<T> {
-    fn clone(&self) -> Self {
-        Self::default()
-    }
-}
-
-impl<T: 'static + Send + Sync> PartialEq for CommandScheduleLabel<T> {
-    fn eq(&self, _: &Self) -> bool {
-        true
-    }
-}
-
-impl<T: 'static + Send + Sync> Eq for CommandScheduleLabel<T> {}
-
-impl<T: 'static + Send + Sync> Hash for CommandScheduleLabel<T> {
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        state.write_u8(0);
-    }
-}
-
-impl<T: 'static + Send + Sync> Debug for CommandScheduleLabel<T> {
-    fn fmt(&self, _: &mut Formatter<'_>) -> Result {
-        Result::Ok(())
-    }
-}
-
-trait RegisterCommand {
-    fn register_command<T: 'static + Send + Sync, P>(
-        &mut self,
-        system: impl IntoSystemConfig<P>,
-    ) -> &mut Self;
-}
-
-impl RegisterCommand for App {
-    fn register_command<T: 'static + Send + Sync, P>(
-        &mut self,
-        system: impl IntoSystemConfig<P>,
-    ) -> &mut Self {
-        self.init_schedule(CommandScheduleLabel::<T>::default())
-            .add_system_to_schedule(CommandScheduleLabel::<T>::default(), system)
-    }
-}
+use self::system_command::{CommandIn, RegisterCommand, SystemCommand};
 
 fn main() {
     App::new()
---
+++ src/system_command.rs
@@ -0,0 +1,79 @@
+use std::{
+    fmt::{Debug, Formatter, Result},
+    hash::{Hash, Hasher},
+    marker::PhantomData,
+};
+
+use bevy::{
+    ecs::{schedule::ScheduleLabel, system::Command},
+    prelude::*,
+};
+
+pub struct SystemCommand<T: 'static + Send + Sync>(pub T);
+
+impl<T: 'static + Send + Sync> Command for SystemCommand<T> {
+    fn write(self, world: &mut World) {
+        world.insert_resource(CommandIn(self.0));
+        world.run_schedule(CommandScheduleLabel::<T>::default());
+        world.remove_resource::<CommandIn<T>>();
+    }
+}
+
+#[derive(Resource)]
+pub struct CommandIn<T: 'static + Send + Sync>(pub T);
+
+#[derive(ScheduleLabel)]
+struct CommandScheduleLabel<T: 'static + Send + Sync> {
+    phantom_data: PhantomData<T>,
+}
+
+impl<T: 'static + Send + Sync> Default for CommandScheduleLabel<T> {
+    fn default() -> Self {
+        Self {
+            phantom_data: PhantomData,
+        }
+    }
+}
+
+impl<T: 'static + Send + Sync> Clone for CommandScheduleLabel<T> {
+    fn clone(&self) -> Self {
+        Self::default()
+    }
+}
+
+impl<T: 'static + Send + Sync> PartialEq for CommandScheduleLabel<T> {
+    fn eq(&self, _: &Self) -> bool {
+        true
+    }
+}
+
+impl<T: 'static + Send + Sync> Eq for CommandScheduleLabel<T> {}
+
+impl<T: 'static + Send + Sync> Hash for CommandScheduleLabel<T> {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        state.write_u8(0);
+    }
+}
+
+impl<T: 'static + Send + Sync> Debug for CommandScheduleLabel<T> {
+    fn fmt(&self, _: &mut Formatter<'_>) -> Result {
+        Result::Ok(())
+    }
+}
+
+pub trait RegisterCommand {
+    fn register_command<T: 'static + Send + Sync, P>(
+        &mut self,
+        system: impl IntoSystemConfig<P>,
+    ) -> &mut Self;
+}
+
+impl RegisterCommand for App {
+    fn register_command<T: 'static + Send + Sync, P>(
+        &mut self,
+        system: impl IntoSystemConfig<P>,
+    ) -> &mut Self {
+        self.init_schedule(CommandScheduleLabel::<T>::default())
+            .add_system_to_schedule(CommandScheduleLabel::<T>::default(), system)
+    }
+}
